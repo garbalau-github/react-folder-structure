ad752bf35388339efe566091a3cd3762b4ab1419
Nick Garbalau <garbalaunick@gmail.com>
2023-04-09 13:11:05 +0300

    Minimal folder structure to run the application

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const rootElement = document.getElementById('root') as HTMLElement;
+
+ReactDOM.createRoot(rootElement).render(<React.StrictMode></React.StrictMode>);
